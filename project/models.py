9cc9343b857c7e644874de861c6c3854086fd1fc
User <lenovnikolas@gmail.com>
2023-03-09 19:09:05 +0200

    Добавлены модели, написаны APi эндпоинты

--- project/models.py
+++ project/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
 from project.setup.db import models
 
@@ -7,3 +7,33 @@ class Genre(models.Base):
     __tablename__ = 'genres'
 
     name = Column(String(100), unique=True, nullable=False)
+
+
+class Director(models.Base):
+    __tablename__ = 'directors'
+
+    name = Column(String(100), unique=True, nullable=False)
+
+
+class Movie(models.Base):
+    __tablename__ = 'movies'
+
+    id = Column(Integer(), primary_key=True, unique=True, nullable=False)
+    title = Column(String(255), nullable=False)
+    description = Column(String(255), nullable=False)
+    trailer = Column(String(255), nullable=False)
+    year = Column(Integer(), nullable=False)
+    rating = Column(Float(), nullable=False)
+    genre_id = Column(Integer(), ForeignKey("genre.id"), nullable=False)
+    director_id = Column(Integer(), ForeignKey("director.id"), nullable=False)
+
+
+class User(models.Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True, unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    name = Column(String(100))
+    surname = Column(String(100))
+    favorite_genre = Column(String(100))
--- project/views/main/__init__.py
+++ project/views/main/__init__.py
@@ -1,5 +1,11 @@
 from .genres import api as genres_ns
+from .movies import api as movies_ns
+from .directors import api as directors_ns
+from .users import api as users_ns
 
 __all__ = [
     'genres_ns',
+    'movies_ns',
+    'directors_ns',
+    'users_ns',
 ]
